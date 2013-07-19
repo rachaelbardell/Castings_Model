2708644ff60749dd7582ab977065bb5862c33983
rachaelbardell <rbardell@akoyainc.com>
2013-07-19 16:42:52 -0500

    latest version

--- server.R
+++ server.R
@@ -1,49 +1,67 @@
 library(shiny)
 library(ggplot2)
 
-#priceData <- parts_df
-# I don't think preprocesing has been done... like factors 
-# log of price has already been taken
-# how is a price zero? cannot take the log of these. Should these be taken out of the model?
-# if we take the log of price in the model.... not all negative log(price) is bad... what if a part cost 2 cents
-# so 1) there are negative price numbers in the data set being used to trai the model
-# and 2) filtering (bounding) the training to non-negative is not right...
-
-priceData <- subset(parts_df, price >= 0)
-# cannot have negative price, when we took the log of price the parts with price = 0 became negative
+# Returns true/false for each column in the data frame
+# For graphing purposes, if there are more than 200 unique values the variable 
+# is treated as continuous, less than 200 will be treated as categorical
+test <- apply(priceData[,1:ncol(priceData)], 2, function(X)length(unique(X)) < 200)
 
 # Define server logic required to plot various variables against price
 shinyServer(function(input, output) {
   
-#  formulaText <- reactive({
-#    paste("price ~ ", input$variable)
-#  })
-  
-  output$caption <- renderText({
-    paste("Price ~ ", input$variable)
-  })
+  dataInput <- reactive({
+          # price cannot be negative
+          priceData <- subset(parts_df, price >= 0)
+          
+          if(input$family != "Do Not Filter by Family") {
+          priceData <- subset(priceData, priceData$family == input$family)}
+          
+          if(input$rsf != "Do Not Filter by RSF") { # case sensitive
+          priceData <- subset(priceData, priceData$rsf == input$rsf) }
+            
+          if(input$logy) {priceData$price <- log(priceData$price)}
+          
+          if(input$logx) {
+            ifelse(test[[input$variable]],
+                   ,
+                   priceData[,input$variable] <- log(priceData[, input$variable]+1))
+                                }
+          
+          return(priceData)
+                        })
   
   output$Plot <- renderPlot({
-    # if statements for filtering. there is an example in this code http://www.rstudio.com/shiny/
+    priceData <- dataInput()
+    # scatterplot vs price if the variable is continuous
+    # bar graph with counts if the variable is categorical
+    ifelse(test[[input$variable]],
+            p <- ggplot(priceData) + geom_bar(aes_string(x = input$variable)) + ggtitle(input$variable) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Count"), # add binwidth = input$n to use binwidth slider        
+            p <- ggplot(priceData) + geom_point(aes_string(x = input$variable, y = "price"), alpha = 0.3) + stat_smooth(aes_string(x = input$variable, y = "price"), color="red", se = "FALSE") + ggtitle(paste("Price vs ", input$variable)) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Price")
+          )
     
-    if(input$rsf != "Do Not Filter by RSF") { # case sensitive
-      priceData <- subset(priceData, priceData$rsf == input$rsf) }
-    
-    # still need to filter by family... need fewer families - they seem redundant
-    # can we combine some together?
-    
-    X <- as.character(input$variable)
-    p <- ggplot() + geom_point(aes(x=priceData[, X], y=priceData$price), alpha=.3)+ ggtitle(paste("Price vs ", input$variable)) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Price")
-    #plot(as.formula(formulaText()), data = priceData, xlab = paste0(input$variable, "\nNumber of Observations Plotted: ", nrow(priceData))) # this works...
     print(p)
-    
-    # plot does not change, it stays as boxvol (what I defined X to be outside of the server)
-    # but the names change. and both use X...
-    
+                            })
+  
+  output$Table <- renderTable({
+    priceData <- dataInput()
+    # prints a table with all of the observations if the variable is continuous
+    # and prints a table of the counts if the variable is categorical
+    ifelse(test[[input$variable]],
+            {tab <- table(priceData[, input$variable])
+            return(tab)},
+            {tab <- priceData[, input$variable]
+            return(as.table(tab))})
   })
   
+  output$caption <- renderText({
+    paste0("Summary of ", input$variable)
+                              })      
+  
+  output$Summary <- renderPrint({
+    priceData <- dataInput()
+    summary(priceData[, input$variable])
+                                })
 })
-
 
 # run the castings model app with the following code
 # runApp("~/Documents/Intern_Model/Shiny")
--- ui.R
+++ ui.R
@@ -1,51 +1,62 @@
 library(shiny)
 
-# -----------
 # Define UI for castings model application
 # have to have headerPanel(), sidebarPanel(), and mainPanel()
+
+# all families with over 30 parts in that family to be displayed in a drop down menu
+families <- table(priceData$family)
+families <- families[families > 30]
+families <- c("Do Not Filter by Family", names(families))
+
+#test <- apply(priceData[,1:ncol(priceData)], 2, function(X)length(unique(X)) < 200)
+# return a list of variables that have more than 200 unique outcomes
+#transformx <- sapply(names(priceData), function(x)if(test[[x]]=="FALSE") x)
+
 shinyUI(pageWithSidebar(
        headerPanel("Castings Model"),
         
         # Sidebar with controls to choose a variable to plot against price and filtering options
         sidebarPanel(
-          # only variables that are in the final model...
-          selectInput("variable", "Variable:", list("Box Volume"="boxvol",
-                                                    "Depth"="depth",
-                                                    "Height"="height",
-                                                    "Width"="width",
-                                                    "Balance"="balance",
-                                                    "Surface Area"= "surfacea",
-                                                    "Parting Line Perimeter"="partinglineperim",
-                                                    "Drill Holes"= "drillholes",
-                                                    "Pressure Air Test" = "pressureairtest",
-                                                    "Mold Complexity" = "moldcomplexity",
-                                                    "Heattreatspecbins" = "heattreatspecbins",
-                                                    "Matspecbins" = "matspecbins",
-                                                    "Demand" = "transformed.demand")                   
-                      ),
-         
-          selectInput("family", "Family", list("Adapters"="ADAPTERS",
-                                               "Process Capable Casting" = "ANY PROCESS CAPABLE CASTING",
-                                               "Bearing Cages", "BEARING CAGES",
-                                               "Bearing Caps"= "BEARING CAPS",
-                                               "Bodies"= "BODIES - INCLUDES PUMP BODIES AND PUMP MANIFOLDS",
-                                               "Bonnets"="BONNETS",
-                                               "Brackets, Braces, and Supports"= "BRACKETS, BRACES AND SUPPORTS",
-                                               "Covers"= "COVERS - EXCEPT TRANSMISSION COVERS",
-                                               "Carriers" = " DIFFERENTIAL CARRIER / PLANET CARRIEERS / CLUTH CARRIERS",
-                                               "Block and Head Castings"= "FERROUS NONFERROUS BLOCK AND HEAD CASTINGS"
-                                               # haven't finished these...
-                                               )),
+          selectInput("variable", "Variable:", list("Box Volume"="boxvol", # 9622
+                                                    "Depth"="depth", # 6408
+                                                    "Height"="height", # 7630
+                                                    "Width"="width", # 7137
+                                                    "Weight"="weight", # 8663
+                                                    "Core Volume"="corevol", # 10073
+                                                    "Number of Ports"="ports", # integer 13
+                                                    "Port Volume"="portvol", # 210?
+                                                    "Balance"="balance", # 19
+                                                    "Surface Area"= "surfacea", # 10074
+                                                    "Parting Line Perimeter"="partinglineperim", # 8724
+                                                    "Drill Holes"= "drillholes", # integer 136
+                                                    "Drill Hole Volume"="drillholevol", # 6318
+                                                    "Assembly"="is_assembly", # logical 2
+                                                    "Ex Gear Count"="exgearcount", # integer 6
+                                                    "Water Pressure Test"="pressuretestwater", # integer 26
+                                                    "Air Pressure Test" = "pressuretestair", # integer 40
+                                                    "Mold Complexity" = "moldcomplexity", # 10690
+                                                    "Heattreatspecbins" = "heattreatspecbins", # character 13
+                                                    "Cleaning Spec"="cleaningspec", # character 20
+                                                    "Brazed Welded Spec Bins"="brazedweldedspecbins", # character 2
+                                                    "Matspecbins" = "matspecbins", # character 40
+                                                    "Demand" = "transformed.demand") # 766                   
+                                                    ),
+          wellPanel(
+          selectInput("family", "Family", families),
           
-          selectInput("rsf", "Filter by RSF?", list("Do Not Filter by RSF", "F", "R"))
-         
-        ),
-        
+          selectInput("rsf", "Filter by RSF?", list("Do Not Filter by RSF", "F", "R")),
+          
+          checkboxInput("logy", "Log Transformation of Price", TRUE),
+          
+          #conditionalPanel(condition= "input.variable in transformx",
+                           checkboxInput("logx", "Transformation of X", FALSE)#,
+          #                 #sliderInput("n", "Bin Width", value = 2, min = 1, max = 100)
+          #                 )
+                    )),    
+       
         mainPanel(
-          h3(textOutput("caption")),
-          plotOutput("Plot")
-          )
+          tabsetPanel(
+              tabPanel("Plot", plotOutput("Plot")),
+              tabPanel("Summary", h5(textOutput("caption")), verbatimTextOutput("Summary"), tableOutput("Table"))
+                  ))
 ))
-
-# how to use the them (rsf and family) as filters...
-# may have to go through familyname column and combine some in R??? 
