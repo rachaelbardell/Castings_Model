6015cfab83b6393161a2547499845dee4675ddab
rachaelbardell <rachael.bardell@gmail.com>
2013-08-02 11:21:33 -0500

    Box Cox transformation

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 library(caret)
+library(corrplot)
 
 # Returns true/false for each column in the data frame
 # For graphing purposes, if there are more than 200 unique values the variable 
@@ -15,72 +16,97 @@ shinyServer(function(input, output) {
   
   dataInput <- reactive({
           
-          priceData <- subset(parts.df, parts.df$inModel, price >= 0)
+        priceData <- subset(parts.df, parts.df$inModel, price >= 0)
           
-          if(input$family != "Do Not Filter by Family") {
-          priceData <- subset(priceData, priceData$family == input$family)}
-          
-          if(input$rsf != "Do Not Filter by RSF") { # case sensitive
-          priceData <- subset(priceData, priceData$rsf == input$rsf) }
+        if(input$family != "Do Not Filter by Family") {
+        priceData <- subset(priceData, priceData$family == input$family)}
+        
+        if(input$medium != "Do Not Filter by Medium") {
+        priceData <- subset(priceData, priceData$medium == input$medium)}
+        
+        if(input$rsf != "Do Not Filter by RSF") { # case sensitive
+        priceData <- subset(priceData, priceData$rsf == input$rsf) }
                 
-          if(input$logy){priceData$price <- log(priceData$price)}
+        if(input$logy){priceData$price <- log(priceData$price)}
           
-          return(priceData)
+        return(priceData)
                         })
-  
+    
   output$Plot <- renderPlot({
-    priceData <- dataInput()
-    ifelse(test[[input$variable]],
-           # if true (ie the variable is categorical) construct a bar graph with counts
-           p <- ggplot(priceData) + geom_bar(aes_string(x = input$variable), binwidth = input$binwidth) + ggtitle(input$variable) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Count"), # add binwidth = input$n to use binwidth slider        
-           # if false (ie the variable is continuous) construct a scatterplot
-           {if(input$logx) priceData[, input$variable] <- log(priceData[, input$variable])
-                            p <- ggplot(priceData) + geom_point(aes_string(x = input$variable, y = "price"), alpha = 0.3) + stat_smooth(aes_string(x = input$variable, y = "price"), color="red", se = "FALSE") + ggtitle(paste("Price vs ", input$variable)) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Price")}
-          )
-    print(p)
+        priceData <- dataInput()
+        ifelse(test[[input$variable]],
+              # if true (ie the variable is categorical) construct a bar graph with counts
+              p <- ggplot(priceData) + geom_bar(aes_string(x = input$variable), binwidth = input$bin_or_logx) + ggtitle(input$variable) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Count"),        
+              # if false (ie the variable is continuous) construct a scatterplot
+              {
+              if(input$transx) {trans <- BoxCoxTrans(priceData[, input$variable]) 
+                                priceData[, input$variable] <- predict(trans, priceData[, input$variable])}
+              p <- ggplot(priceData) + geom_point(aes_string(x = input$variable, y = "price"), alpha = 0.3) + stat_smooth(aes_string(x = input$variable, y = "price"), color="red", se = "FALSE") + ggtitle(paste("Price vs ", input$variable)) + xlab(paste0(input$variable, "\n\nNumber of Observations Plotted: ", nrow(priceData))) + ylab("Price")}
+              # BoxCox transformation of input variable
+              )
+        print(p)
                             })
+  
+  # lambda = 0 is equivalent to a log transformation
+  output$lambda <- renderText({
+        if(!test[[input$variable]] && input$transx){ # have to have two arguments so that when input$transx does not exist it does not display an error. now it just displays nothing
+        trans <- BoxCoxTrans(priceData[ ,input$variable])
+        lambda <- trans$lambda
+        return(paste("lambda = ", lambda))}
+                                })
+ 
+  # slider bar for binwidth if a bar graph is made (and the factors are numeric)
+  # log(x) transformation checkbox if a scatterplot is made
+  output$bin_or_logx <- renderUI({
+        priceData <- dataInput()
+        if(input$variable %in% binSlider) {
+              max <- max(priceData[, input$variable])
+              min <- min(priceData[, input$variable])
+              bin <- (max - min) / 30
+              if(max <= 10) bin = 1
+        return(sliderInput("bin_or_logx", "Binwidth", min = round(min), max = round(max/5), value = round(bin, 1)))}#, step = round((max-min)/5, 2)))}
+        if(!test[[input$variable]]) return(checkboxInput("transx", "Box Cox Transformation of X", FALSE))
+                                  
+                                  })  
   
   # histogram of the input variable
   output$Histogram <- renderPlot({
-    priceData <- dataInput()
-    if(input$logx) priceData[, input$variable] <- log(priceData[, input$variable])
-    p <- ggplot(priceData)+geom_histogram(aes_string(x=input$variable))+ggtitle(paste("Histogram of " ,input$variable))
-    print(p)
+        priceData <- dataInput()
+        if(!test[[input$variable]] && input$transx) {trans <- BoxCoxTrans(priceData[, input$variable]) 
+              priceData[, input$variable] <- predict(trans, priceData[, input$variable])}
+        p <- ggplot(priceData)+geom_histogram(aes_string(x=input$variable))+ggtitle(paste("Histogram of " ,input$variable))
+        print(p)
     })
   
   output$Table <- renderTable({
-    priceData <- dataInput()
-    # prints a table with 10 of the observations if the variable is continuous
-    # and prints a table of the counts of the factors if the variable is categorical
-    ifelse(test[[input$variable]],
-            {tab <- table(priceData[, input$variable])
-            return(tab)},
-            {if(input$logx) priceData[, input$variable] <- log(priceData[, input$variable])
-             tab <- priceData[1:10, input$variable]
-             return(as.table(tab))})
+        priceData <- dataInput()
+        # prints a table with 10 of the observations if the variable is continuous
+        # and prints a table of the counts of the factors if the variable is categorical
+        ifelse(test[[input$variable]],
+              {tab <- table(priceData[, input$variable])
+              return(tab)},
+              {if(!test[[input$variable]] && input$transx) {trans <- BoxCoxTrans(priceData[, input$variable]) 
+                      priceData[, input$variable] <- predict(trans, priceData[, input$variable])}
+              tab <- priceData[1:10, input$variable]
+              return(as.table(tab))})
   })
   
   output$caption <- renderText({paste("Summary of ", input$variable)})      
   
   # summary of the input variable
   output$Summary <- renderPrint({
-    priceData <- dataInput()
-    if(input$logx) priceData[, input$variable] <- log(priceData[, input$variable])
-    summary(priceData[, input$variable])
-                                })
+        priceData <- dataInput()
+        if(!test[[input$variable]] && input$transx) {trans <- BoxCoxTrans(priceData[, input$variable]) 
+              priceData[, input$variable] <- predict(trans, priceData[, input$variable])}
+        summary(priceData[, input$variable])
+                                  })
   
-  # slider bar for binwidth if a bar graph is made (and the factors are numeric)
-  # log(x) transformation checkbox if a scatterplot is made
-  output$bin_or_logx <- renderUI({
-    priceData <- dataInput()
-    if(input$variable %in% binSlider) {
-      max <- max(priceData[, input$variable])
-      min <- min(priceData[, input$variable])
-      bin <- (max - min) / 30
-      return(sliderInput("binwidth", "Binwidth", min = round(min), max = round(max/5), value = round(bin, 0)))}#, step = round((max-min)/5, 1)))}
-    if(!test[[input$variable]]) return(checkboxInput("logx", "Log Transformation of X", FALSE))
-  })
-
+  output$Correlation <- renderPlot({
+        # corr only takes numeric predictors
+        corr <- cor(subset(priceData, select = num))
+        # plot of correlations, reordered to reveal clusters of highly correlated variables
+        print(corrplot(corr, order="hclust"))
+                                    })
 })
 
 # run the castings model app with the following code
--- ui.R
+++ ui.R
@@ -4,42 +4,47 @@ library(shiny)
 # have to have headerPanel(), sidebarPanel(), and mainPanel()
 
 # all families with over 30 parts in that family to be displayed in a drop down menu
-families <- table(priceData$family)
-families <- families[families > 30]
+families <- table(priceData$family) # 105 families
+families <- families[families > 30] # only 43 with over 30 observations
 families <- c("Do Not Filter by Family", names(families))
+
+medium <- table(priceData$medium)
+medium <- c("Do Not Filter by Medium", names(medium))
 
 shinyUI(pageWithSidebar(
        headerPanel("Castings Model"),
         
         # Sidebar with controls to choose a variable to plot against price and filtering options
         sidebarPanel(
-          selectInput("variable", "Variable:", list("Air Pressure Test" = "pressuretestair", # integer 40
-                                                    "Assembly"="is_assembly", # logical 2
-                                                    "Balance"="balance", # 19
-                                                    "Box Volume"="boxvol", # 9622
-                                                    "Brazed Welded Spec Bins"="brazedweldedspecbins", # character 2
-                                                    "Cleaning Spec"="cleaningspec", # character 20
-                                                    "Core Volume"="corevol", # 10073
-                                                    "Demand" = "transformed.demand", # 766 
-                                                    "Depth"="depth", # 6408
-                                                    "Drill Holes"= "drillholes", # integer 136
-                                                    "Drill Hole Volume"="drillholevol", # 6318  
-                                                    "Ex Gear Count"="exgearcount", # integer 6
-                                                    "Heattreatspecbins" = "heattreatspecbins", # character 13
-                                                    "Height"="height", # 7630
-                                                    "Matspecbins" = "matspecbins", # character 40
-                                                    "Mold Complexity" = "moldcomplexity", # 10690
-                                                    "Number of Ports"="ports", # integer 13
-                                                    "Parting Line Perimeter"="partinglineperim", # 8724                              
-                                                    "Port Volume"="portvol", # 210?
-                                                    "Surface Area"= "surfacea", # 10074
-                                                    "Water Pressure Test"="pressuretestwater", # integer 26
-                                                    "Weight"="weight", # 8663                               
-                                                    "Width"="width") # 7137                  
-                                                    ),  
+              selectInput("variable", "Variable:", list("Air Pressure Test" = "pressuretestair", # integer 40
+                                                        "Assembly"="is_assembly", # logical 2
+                                                        "Balance"="balance", # 19
+                                                        "Box Volume"="boxvol", # 9622
+                                                        "Brazed Welded Spec Bins"="brazedweldedspecbins", # character 2
+                                                        "Cleaning Spec"="cleaningspec", # character 20
+                                                        "Core Volume"="corevol", # 10073
+                                                        "Demand" = "transformed.demand", # 766 
+                                                        "Depth"="depth", # 6408
+                                                        "Drill Holes"= "drillholes", # integer 136
+                                                        "Drill Hole Volume"="drillholevol", # 6318  
+                                                        "Ex Gear Count"="exgearcount", # integer 6
+                                                        "Heattreatspecbins" = "heattreatspecbins", # character 13
+                                                        "Height"="height", # 7630
+                                                        "Matspecbins" = "matspecbins", # character 40
+                                                        "Mold Complexity" = "moldcomplexity", # 10690
+                                                        "Number of Ports"="ports", # integer 13
+                                                        "Parting Line Perimeter"="partinglineperim", # 8724                              
+                                                        "Port Volume"="portvol", # 210?
+                                                        "Surface Area"= "surfacea", # 10074
+                                                        "Water Pressure Test"="pressuretestwater", # integer 26
+                                                        "Weight"="weight", # 8663                               
+                                                        "Width"="width") # 7137                  
+                          ),  
           wellPanel(
           h5("Filtering:"),
           selectInput("family", "Family", families),
+          
+          selectInput("medium", "Medium", medium),
           
           selectInput("rsf", "RSF", list("Do Not Filter by RSF", "F", "R"))),
           
@@ -49,8 +54,11 @@ shinyUI(pageWithSidebar(
           ),  
        
         mainPanel(
-          tabsetPanel(
-              tabPanel("Plot", plotOutput("Plot")),
-              tabPanel("Summary", h5(textOutput("caption")), verbatimTextOutput("Summary"), plotOutput("Histogram"), tableOutput("Table"))
-                  ))
+              tabsetPanel(
+                  tabPanel("Plot", plotOutput("Plot"), verbatimTextOutput("lambda")),
+                  tabPanel("Diagnostics", h5("Correlation Matrix"), plotOutput("Correlation")),
+                  tabPanel("Summary", h5(textOutput("caption")), verbatimTextOutput("Summary"), plotOutput("Histogram"), tableOutput("Table"))
+                          ))
 ))
+
+# when we pick a model... residual plot, pred vs actual, etc
