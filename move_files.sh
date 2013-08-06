d3b5b1fe8e751d98fa12ccecb02ab820c4131afa
rachaelbardell <rachael.bardell@gmail.com>
2013-08-06 14:42:00 -0500

    update

--- move_files.sh
+++ move_files.sh
@@ -1,3 +1,1 @@
-cp /src/Castings_Model/server.R /var/shiny-server/www
-cp /src/Castings_Model/ui.R /var/shiny-server/www
-cp /src/Castings_Model/castings_data.csv /var/shiny-server/www
+cp -a Castings_Model /var/shiny-server/www
